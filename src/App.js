5c127ad0a82641d37ce125a9ad7db94a22e8782a
Praveenkumar <111970676+ipraveenvasanth@users.noreply.github.com>
2024-01-19 13:59:48 +0530

    ryur6u

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Modal from './Modal'
+import Dropdown from './Dropdown'
+
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <View style={{flex:0.075,backgroundColor:"#363E8E",justifyContent:"center",alignItems:"flex-start",paddingLeft:10}}>
+      <View style={{ flex: 0.075, backgroundColor: "#363E8E", justifyContent: "center", alignItems: "flex-start", paddingLeft: 10 }}>
         <Header />
       </View>
-      <View style={{flex:0.9}}>
-        <Modal/>
+      <View>
+        <Dropdown />
       </View>
-      <View style={{flex:0.2,backgroundColor:"#363E8E",justifyContent:"center",alignItems:"flex-start",paddingLeft:10}}>
+      <View style={{ flex: 0.9 }}>
+        <Modal />
+      </View>
+      <View style={{ flex: 0.2, backgroundColor: "#363E8E", justifyContent: "center", alignItems: "flex-start", paddingLeft: 10 }}>
         <Footer />
       </View>
     </View>
---
+++ src/Dropdown.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+// import AntDesign from '@expo/vector-icons/AntDesign';
+
+const data = [
+    { label: 'A3 SIZE GLOSSY LAMINATION', value: '1' },
+    { label: 'A3 SIZE MATT LAMINATION', value: '2' },
+    { label: 'A3 SIZE DRIPOFF', value: '3' },
+    { label: 'A3 SIZE MATT LAMINATION', value: '4' },
+    { label: 'A3 SIZE DRIPOFF', value: '5' },
+    { label: 'A3 SIZE MATT LAMINATION', value: '6' },
+    { label: 'A3 SIZE DRIPOFF', value: '7' },
+];
+
+const App = () => {
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+
+    const renderLabel = () => {
+        if (isFocus) {
+            return (
+                <Text style={[styles.label, !isFocus && { color: 'black' }]}>
+                    Select product here
+                </Text>
+            );
+        }
+        return null;
+    };
+
+    const renderItem = item => {
+        return (
+            <View style={styles.item}>
+                <Text style={styles.textItem}>{item.label}</Text>
+                {item.value === value && (
+                    <Image source={{ uri: "https://w7.pngwing.com/pngs/752/449/png-transparent-at-sign-computer-icons-radio-button-miscellaneous-monochrome-black-thumbnail.png" }} style={styles.icon} />
+                )}
+            </View>
+        );
+    };
+
+    return (
+        <View style={styles.container}>
+            {renderLabel()}
+            <Dropdown
+                style={styles.dropdown}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={data}
+                search
+                maxHeight={500}
+                labelField="label"
+                valueField="value"
+                // placeholder="Select product here"
+                searchPlaceholder="Search..."
+                value={value}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={item => {
+                    setValue(item.value);
+                    setIsFocus(false);
+                }}
+                renderLeftIcon={() => (
+                    <Image source={{ uri: "https://rukminim2.flixcart.com/image/850/1000/kkwwu4w0/art-craft-kit/6/v/e/a4-3d-plastic-sheet-edumart-original-imagy5h4h5mghfgu.jpeg?q=90&crop=false" }} style={styles.img1} />
+                )}
+                renderItem={renderItem}
+            />
+        </View>
+    );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        padding: 1,
+    },
+    label: {
+        position: 'absolute',
+        backgroundColor: 'white',
+        borderRadius: 5,
+        left: 135,
+        top: 8,
+        zIndex: 999,
+        paddingHorizontal: 8,
+        fontSize: 14,
+        color: "grey"
+    },
+    dropdown: {
+        margin: 16,
+        height: 50,
+        backgroundColor: 'white',
+        // borderColor: 'orange',
+        borderWidth: 1,
+        borderRadius: 12,
+        padding: 12,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+
+        elevation: 2,
+    },
+    img1: {
+        width: 25,
+        height: 28,
+        resizeMode: "cover",
+        marginRight: 10
+    },
+    icon: {
+        width: 15,
+        height: 18,
+        resizeMode: "contain",
+    },
+    item: {
+        padding: 17,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    textItem: {
+        flex: 1,
+        fontSize: 16,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+        color: "black",
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+
+    },
+});
