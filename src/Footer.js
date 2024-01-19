9e9de1357a5349ba83c253b437da8857e9134b5a
Sai sham <saisham2605@gmail.com>
2024-01-19 13:53:29 +0530

    a

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,6 @@ const Footer = () => {
       <Text style={{color:"white",fontSize:20,fontWeight:"bold",marginTop:2}}>Sivakasi  +91 98947 10050</Text>
       <Text numberOfLines={2} ellipsizeMode='tail' style={{color:"white",fontSize:14,fontWeight:"500",marginTop:2}}>1/4, Sattur Road, Near Royal Travels, Opp. Old Rto Office, Sivakasi, Virudhunagar-626189, Tamil Nadu, India
       </Text>
-
     </View>
   )
 }
